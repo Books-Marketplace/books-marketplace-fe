a3da5de13c0e23a28f874300072b99ea1c85b5b0
jessernasraoui <jassernassraoui@gmail.com>
2025-05-14 21:15:13 +0100

    create navbar component and footer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'navbar',
+    loadComponent: () =>
+      import('./Navbar/navbar/navbar.component').then((m) => m.NavbarComponent),
+  },
+  {
+    path: 'footer',
+    loadComponent: () =>
+      import('./Footer/footer/footer.component').then((m) => m.FooterComponent),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
